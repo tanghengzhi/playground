5bab683e7ea154d410c2f0efeb3e856870d94302
Victor Tang <tanghengzhi@live.com>
2019-05-08 10:59:13 +0800

    Implement Bubble Sort

---
+++ algorithm/sort/bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+    console.log(array);
+  }
+
+  return array;
+}
+
+array = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+
+bubbleSort(array);
